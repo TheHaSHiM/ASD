eacef6a0f815bd9dde041577d8ad62fe33c4dfbb
TheHaSHiM <TheHaSHiM@users.noreply.github.com>
2025-11-09 16:54:35 +0200

    fix

--- lab02/1lab02.c
+++ lab02/1lab02.c
@@ -7,17 +7,20 @@ int main(void)
     int func = 0;
     printf("Enter an integer n: ");
     scanf("%d", &n);
+    double two_pow_i = 1.0;
     for (int i = 1; i <= n; i++)
     {
         double P = 1.0;
         for (int j = 1; j <= i; j++)
         {
-            P = P * (j + 2) / 10;
+            P = P * (j + 2.0) / 10.0;
             func += 3;
         }
-
-        S = S + P / (i << i);
+        two_pow_i *= 2;
+        func += 1;
+        S = S + P /(i* two_pow_i);
         func += 3;
+        
     }
     printf("S = %.7f\n", S);
     printf("Number of operations: %d\n", func);
--- lab02/2lab02.c
+++ lab02/2lab02.c
@@ -8,15 +8,18 @@ int main(void)
     double S = 0;
     double P = 1.0;
     int func = 0;
+    double two_pow_i = 1.0;
     for (int i = 1; i <= n; i++)
     {
+        two_pow_i *= 2;
+        func += 1;
         P = P * (i + 2) / 10;
-        S = S + P / (i << i);
-        func += 6;
+        func += 3;
+        S = S + P / (i * two_pow_i);
+        func += 3;
     }
 
     printf("S = %.7f\n", S);
     printf("Number of operations: %d\n", func);
     return 0;
 }
-А
